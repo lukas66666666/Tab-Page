611f9d0d11632e96235e44ccf5d44822ed6a69f8
lukas66666666 <lukas.cada007@gmail.com>
2026-07-23 11:10:56 +0200

    add clock and date

---
+++ src/clock.js
@@ -0,0 +1,23 @@
+const clockContainer = document.getElementById("clockContainer");
+
+function updateTime() {
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    minutes = checkTime(minutes);
+    let month = date.getMonth();
+    let day = date.getDate();
+    let dayOfWeek = date.getDay();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let clock = `<p id="clock">${hours}:${minutes}</p>
+    <p id="date">${days[dayOfWeek]} ${day}.${month + 1}.</p>`;
+    clockContainer.innerHTML = clock;
+    setTimeout(updateTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
+
+updateTime();
